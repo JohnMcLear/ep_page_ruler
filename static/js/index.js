f0c91d6cb30c11b3c71c5582bd1c3339a8910753
John McLear <john@mclear.co.uk>
2013-11-28 16:11:48 +0000

    workign in FF but realization need to add styling to div not span could be very painful

--- static/js/index.js
+++ static/js/index.js
@@ -3,40 +3,16 @@ var ruler = {};
 
 var $ = require('ep_etherpad-lite/static/js/rjquery').$;
 var _ = require('ep_etherpad-lite/static/js/underscore');
-var headingClass = 'heading';
-var cssFiles = ['ep_headings/static/css/editor.css'];
-
-// All our tags are block elements, so we just return them.
-var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'];
-var aceRegisterBlockElements = function(){
-  return tags;
-}
+var rulerClass = 'ruler';
 
 // Bind the event handler to the toolbar buttons
-var postAceInit = function(hook, context){
-  /*
-  var hs = $('#heading-selection');
-  hs.on('change', function(){
-    var value = $(this).val();
-    var intValue = parseInt(value,10);
-    if(!_.isNaN(intValue)){
-      context.ace.callWithAce(function(ace){
-        ace.ace_doInsertHeading(intValue);
-      },'insertheading' , true);
-      hs.val("dummy");
-    }
-  })
-  */
-
-
+exports.postAceInit = function(hook, context){
   setTimeout(function(){
-    ruler.init()
+    ruler.init(context)
   }, 200);
-
-
 };
 
-ruler.init = function(){
+ruler.init = function(context){
   var pageWidth = $("#editorcontainer > iframe").outerWidth();
   var pageOffset = $("#editorcontainer > iframe").offset().left || 0;
   var innerWidth = $('iframe[name="ace_outer"]').contents().find('iframe').width();
@@ -50,89 +26,99 @@ ruler.init = function(){
   $('#ep_page_ruler_left_container').click(function(e){
     var pageOffset = $("#editorcontainer > iframe").offset().left || 0;
     $('#ep_page_ruler_left').css("left", (e.clientX-pageOffset-114) +"px");
+    context.ace.callWithAce(function(ace){
+      ace.ace_doInsertRulerLeft(e.clientX-pageOffset-114);
+    },'insertRulerLeft' , true);
   });
+
   $('#ep_page_ruler_right_container').click(function(e){
     var pageOffset = $("#editorcontainer > iframe").offset().left || 0;
     $('#ep_page_ruler_right').css("left", (e.clientX-$("#ep_page_ruler_right_container").offset().left) +"px");
+    context.ace.callWithAce(function(ace){
+      ace.ace_doInsertRulerRight(e.clientX-$("#ep_page_ruler_right_container").offset().left);
+    },'insertRulerRight' , true);
   });
+
   $('.rulerControl').show();
 }
 
-/*
-function moveRuler(target, e){
-  var pageOffset = $("#editorcontainer > iframe").offset().left;
-  $(target).css("left", (e.screenX-pageOffset) +"px");
-  console.log(target, e);
-}
-*/
-// Our heading attribute will result in a heaading:h1... :h6 class
+// Our ruler attribute will result in a ruler:x class
 function aceAttribsToClasses(hook, context){
-  if(context.key == 'heading'){
-    return ['heading:' + context.value ];
+  if(context.key == 'rulerLeft'){
+    return ['rulerLeft:' + context.value ];
+  }
+  if(context.key == 'rulerRight'){
+    return ['rulerRight:' + context.value ];
   }
 }
 
-// Here we convert the class heading:h1 into a tag
+// Here we convert the class ruler:h1 into a tag
 var aceDomLineProcessLineAttributes = function(name, context){
+
   var cls = context.cls;
   var domline = context.domline;
-  var headingType = /(?:^| )heading:([A-Za-z0-9]*)/.exec(cls);
-  var tagIndex;
+  var rulerLeft = /(?:^| )rulerLeft:([A-Za-z0-9]*)/.exec(cls);
+  var rulerRight = /(?:^| )rulerRight:([A-Za-z0-9]*)/.exec(cls);
+  if(rulerLeft || rulerRight){
+    var formattedString = "";
+    if(rulerLeft){
+       formattedString += 'margin-left:' + rulerLeft[1] + 'px;'
+    }
+    if(rulerRight){ 
+      formattedString += 'padding-right:' +rulerRight[1] + 'px;'
+    }
   
-  if (headingType) tagIndex = _.indexOf(tags, headingType[1]);
-  
-  if (tagIndex !== undefined && tagIndex >= 0){
-    
-    var tag = tags[tagIndex];
     var modifier = {
-      preHtml: '<' + tag + '>',
-      postHtml: '</' + tag + '>',
+      preHtml: '<span style='+formattedString+'>',
+      postHtml: '</span>',
       processedMarker: true
     };
     return [modifier];
+  }else{
+    return [];
   }
-  return [];
 };
 
-// Find out which lines are selected and assign them the heading attribute.
-// Passing a level >= 0 will set a heading on the selected lines, level < 0 
+// Find out which lines are selected and assign them the ruler attribute.
+// Passing a level >= 0 will set a ruler on the selected lines, level < 0 
 // will remove it
-function doInsertHeading(level){
+function doInsertRulerLeft(level){
   var rep = this.rep,
     documentAttributeManager = this.documentAttributeManager;
-  if (!(rep.selStart && rep.selEnd) || (level >= 0 && tags[level] === undefined))
-  {
-    return;
-  }
-  
+
   var firstLine, lastLine;
   
   firstLine = rep.selStart[0];
   lastLine = Math.max(firstLine, rep.selEnd[0] - ((rep.selEnd[1] === 0) ? 1 : 0));
   _(_.range(firstLine, lastLine + 1)).each(function(i){
-    if(level >= 0){
-      documentAttributeManager.setAttributeOnLine(i, 'heading', tags[level]);
-    }else{
-      documentAttributeManager.removeAttributeOnLine(i, 'heading');
-    }
+    documentAttributeManager.setAttributeOnLine(i, 'rulerLeft', level);
+  });
+}
+
+// Find out which lines are selected and assign them the ruler attribute.
+// Passing a level >= 0 will set a ruler on the selected lines, level < 0
+// will remove it
+function doInsertRulerRight(level){
+  var rep = this.rep,
+    documentAttributeManager = this.documentAttributeManager;
+
+  var firstLine, lastLine;
+  
+  firstLine = rep.selStart[0];
+  lastLine = Math.max(firstLine, rep.selEnd[0] - ((rep.selEnd[1] === 0) ? 1 : 0));
+  _(_.range(firstLine, lastLine + 1)).each(function(i){
+    documentAttributeManager.setAttributeOnLine(i, 'rulerRight', level);
   });
 }
 
 
-// Once ace is initialized, we set ace_doInsertHeading and bind it to the context
-function aceInitialized(hook, context){
+// Once ace is initialized, we set ace_doInsertruler and bind it to the context
+exports.aceInitialized = function(hook, context){
   var editorInfo = context.editorInfo;
-  editorInfo.ace_doInsertHeading = _(doInsertHeading).bind(context);
+  editorInfo.ace_doInsertRulerLeft = _(doInsertRulerLeft).bind(context);
+  editorInfo.ace_doInsertRulerRight = _(doInsertRulerRight).bind(context);
 }
 
-function aceEditorCSS(){
-  return cssFiles;
-};
-
 // Export all hooks
-exports.aceRegisterBlockElements = aceRegisterBlockElements;
-exports.aceInitialized = aceInitialized;
-exports.postAceInit = postAceInit;
 exports.aceDomLineProcessLineAttributes = aceDomLineProcessLineAttributes;
 exports.aceAttribsToClasses = aceAttribsToClasses;
-exports.aceEditorCSS = aceEditorCSS;
